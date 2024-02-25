dc3d996495970a5bfdca01b51755ead596743c7a
manish koladiya <manishkoladiya546@gmail.com>
2024-02-24 22:17:46 -0500

    Add project files.

---
+++ Manish A2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Here i am defining get and set method
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+// Here i am defining player public class
+public class Player
+{
+    public string Name { get; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+    public int TurnCount { get; set; }
+
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+        TurnCount = 1; // Start counting from 1
+    }
+
+    public void Move(char direction)
+    {
+        TurnCount++;
+
+        switch (direction)
+        {
+            case 'U':
+                Position.Y--;
+                break;
+            case 'D':
+                Position.Y++;
+                break;
+            case 'L':
+                Position.X--;
+                break;
+            case 'R':
+                Position.X++;
+                break;
+        }
+    }
+}
